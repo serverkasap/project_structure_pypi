4fd8d86595c1c8a2959ceb1ee4d29c9c9ec514dd
Server Kasap <49035188+serverkasap@users.noreply.github.com>
2022-07-15 13:01:51 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='celebrity_births_test',
-    version='0.0.1',    
+    version='0.0.1',
     description='Mock package that allows you to find celebrity by date of birth',
     url='https://github.com/IvanYingX/project_structure_pypi.git',
     author='Ivan Ying',
